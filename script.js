d9bc26809a1708c7e347a54eafafa74bc09055cb
kare03 <119617902+kare03@users.noreply.github.com>
2023-04-20 10:37:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function miFuncomp() {
+    swal("Gracias, Adquiriste una Membresía mensual");  }
+
+function miFunc() {
+    swal("Acceso a todas las actividades menos Natación") }
+
+function miFuncompanual() {
+    swal("Gracias, Adquiriste una Membresía Anual");  }
+
+function miFuncan() {
+    swal("Acceso a todas las actividades incluido Natación y la app Nutrición") }
+ 
